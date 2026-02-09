0336c3aabe34334166fa2aa4cd65ea42ecd5c996
KARAN SINGH <karansingh25822@gmail.com>
2026-02-09 23:56:11 +0530

    adding header for the lib open

--- include/lhig/opcua_server.hpp
+++ include/lhig/opcua_server.hpp
@@ -1,18 +1,33 @@
 #ifndef LHIG_OPCUA_SERVER_HPP
 #define LHIG_OPCUA_SERVER_HPP
 
-namespace lhig{
-    class OpcuaServer{
-        public:
-        //constructor
-        OpcuaServer();
-        // method
-        void start();
-        void stop();
-        private:
-        // we have to add opc ua serve var 
+#include <atomic>
+#include <thread>
 
-    };
-}
+// Forward declare the C struct to avoid including the C header here
+struct UA_Server;
+
+namespace lhig {
+
+class OpcuaServer {
+public:
+    OpcuaServer();
+    ~OpcuaServer();
+
+    void start();
+    void stop();
+
+    // This is the API our main loop will use to push data
+    void updateVoltage(double voltage);
+
+private:
+    void runLoop();
+
+    UA_Server* server_;
+    std::atomic<bool> running_;
+    std::thread server_thread_;
+};
+
+} 
 
 #endif
